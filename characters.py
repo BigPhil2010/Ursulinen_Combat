4e13aa413a4d19ad5d8d15c1d9ff2bfcdd8c7392
BigPhil2010 <phil.buerger.2602@gmail.com>
2025-12-10 23:13:08 +0100

    mutke mit buggy werten

--- characters.py
+++ characters.py
@@ -99,8 +99,58 @@ KIEN = {
     "jump_frames_count": 0
 }
 
+
+##########Mutke##########
+
+spritesheet_LO_MUTK = {
+    "run": [[0, 0, 7, 0], 15, True],
+    "idle": [[0, 1, 1, 1], 5, True],
+    "jump": [[0, 3, 5, 3], 5, False],
+    "hit_run": [[0, 4, 6, 4], 15, False],
+    "hit_idle": [[0, 2, 7, 2], 5, False],
+}
+
+MUTK = {
+    "name": "Kienast",
+    "hp": 100,
+    "hp_bar": None,
+    "sprite": None,
+    "flipped_sprite": None,
+    "sprite_sheet": pygame.image.load(r"recources/images/sprites/png/mutke.png"),
+    "sprite_sheet_layout": spritesheet_LO_MUTK,
+    "animation": "run",
+    "hit": False,
+    "damage": 0.4,
+    "damage_done": False,
+    #animations
+    "run": [],
+    "idle": [],
+    "jump": [],
+    "hit_run": [],
+    "hit_idle": [],
+    "frame": 0,
+    "cooldown": 50,
+    "cooldown_count": 0,
+    "looking_left": None,
+    "keyset": None,
+    "rect": None,
+    "start_x": None,
+    "start_y": None,
+    "width": 32,
+    "height": 32,
+    "collision_bottom": False,
+    "collision_left": False,
+    "collision_right": False,
+    "speed": 0.7,
+    "gravity": 1,
+    "jump_power": 1,
+    "jump_frames": 30,
+    "jump_frames_count": 0
+}
+
 ##########LIST##########
 character_list = {
     "RITT": RITT,
-    "KIEN": KIEN
+    "KIEN": KIEN,
+    "MUTK": MUTK
 }
